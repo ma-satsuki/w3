af7ea69613b999b764e47ff96e6e0aab8eec496b
ma-satsuki <109150164+ma-satsuki@users.noreply.github.com>
2023-09-21 11:45:54 +0900

    Update main.js

--- scripts/main.js
+++ scripts/main.js
@@ -1,16 +1,7 @@
 
 // Personalized welcome message code
 
-const myImage = document.querySelector("img");
 
-myImage.onclick = () => {
-  const mySrc = myImage.getAttribute("src");
-  if (mySrc === "images/firefox-icon.png") {
-    myImage.setAttribute("src", "images/firefox2.png");
-  } else {
-    myImage.setAttribute("src", "images/firefox-icon.png");
-  }
-};
 
 
 let myButton = document.querySelector('button');
